32b21183ca6b703a70771e5a69e3b93438521058
MSiesto <miguelsiesto@gmail.com>
2024-01-18 20:26:47 +0100

    login route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const saltRounds = 10;
 const User = require("./../models/User.model");
 
@@ -35,6 +36,36 @@ router.post("/signup", (req, res, next) => {
     .catch((err) => {
       next(err);
     });
+});
+
+router.post("/login", (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (email === "" || password === "") {
+    res.status(400).json({ message: "Provide email and password." });
+    return;
+  }
+
+  User.findOne({ email })
+    .then((foundUser) => {
+      if (!foundUser) {
+        res.status(401).json({ message: "User not found." });
+        return;
+      }
+      if (bcrypt.compareSync(password, foundUser.password)) {
+        const { _id, email, username } = foundUser;
+        const payload = { _id, email, username };
+
+        const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+          algorithm: "HS256",
+          expiresIn: "72h",
+        });
+        res.status(200).json({ authToke: authToken });
+      } else {
+        res.status(401).json({ message: "Unable to authenticate the user" });
+      }
+    })
+    .catch((err) => next(err));
 });
 
 module.exports = router;
